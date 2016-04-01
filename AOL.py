82a7feea4e57c7ed8e30da528812bdf1eea3bcdd
Adam Sinck <absinck+github@grueslayer.com>
2016-04-01 13:28:13 -0700

    initial commit

---
+++ AOL.py
@@ -0,0 +1,79 @@
+import sys, random
+
+import AOL_Keys as AOL
+
+#this can take up to two arguments:
+#chance is the chance of success
+#mod is the "out of"; the default is two, meaning there are two
+#possibilities.
+#if no parameters are provided, then it's a 1/2 (50%) chance of
+#returning true.
+def myRand(chance=1, mod=2):
+    return random.randint(0,20)%mod < chance
+
+#this returns a random index in the given range
+def randIndex(end):
+    return random.randint(0, end-1)
+
+#this returns a random replacement from the translation table
+def randResult(key):
+    if type(AOL.words[key]) == type([]):
+        end = len(AOL.words[key])
+        return AOL.words[key][randIndex(end)]
+    else:
+        return AOL.words[key]
+    
+returnString = ""
+newWord = ""
+
+if sys.argv[1] == "@ignore":
+    returnString = ' '.join(sys.argv[2:])
+else:
+    for word in sys.argv[1:]:
+        newWord = word
+        #90% chance of translating each word
+        if myRand(9,10):
+            if (word.lower() in AOL.words):
+                newWord = randResult(word.lower())
+            else:
+                for substitution in AOL.subs:
+                    #50% chance of substituting letters around, if the
+                    #substitution is in the word
+                    if substitution in newWord and myRand():
+                        newWord = newWord.replace(substitution, AOL.subs[substitution])
+            #50% chance of raising the case
+            if myRand():
+                newWord = newWord.upper()
+        returnString += newWord + " "
+    
+    #swap some letters around
+    buildString = ""
+    for i in range(0, len(returnString)-1, 2):
+        if myRand(1,6):
+            buildString += returnString[i+1] + returnString[i]
+        else:
+            buildString += returnString[i] + returnString[i+1]
+    
+    #add some random acronyms to the end
+    for i in range(randIndex(10)):
+        if myRand(3,4):
+            length = len(AOL.addons)
+            buildString += AOL.addons[randIndex(length)] + " "
+            
+    for i in range(randIndex(20)):
+        if myRand(3,4):
+            if myRand(4,5):
+                buildString += "!"
+            else:
+                buildString += "1"
+    
+    returnString = ""
+    for i in buildString.split():
+        #do some capitalization
+        if myRand():
+            returnString += i.upper() + " "
+        else:
+            returnString += i.lower() + " "
+                    
+print returnString
+
---
+++ AOL_Keys.py
@@ -0,0 +1,57 @@
+#if there's one translation candidate, just use a string
+#if there are multiple possible translations, use an array
+words = {
+    "there":["their", "theyre", "they're"],
+    "their":["there", "theyre", "they're"],
+    "they're":["their", "theyre", "there"],
+    "theyre":["their", "there"],
+
+    "too":["two", "to"],
+    "to":["too", "two"],
+    "two":["too", "to"],
+    "the":["teh", "da"],
+
+    "your":["ur","you're"],
+    "its":"it's",
+    "it's":"its",
+    "right":"rite",
+    "you":"u",
+    "what":"wut",
+    "are":"r",
+    "why":"y",
+    "because":"b/c",
+    "oh":"o",
+    "my":"mah",
+    "with":"wit",
+    "really":"rilly",
+    "please":"plz",
+    "thanks":"thx",
+    "ok":["k","kthx"],
+    "okay":["k","kthx"],
+    "library":"liberry",
+    "be":"b",
+    "schoool":"skool",
+    "hi":"hai",
+    "noob":"newb",
+    "hack":"h4x",
+    "hacker":"h4x",
+    "hacked":"h4x",
+    "lag":"h4x",
+    "cheat":"chiet",
+    "that":"taht",
+    "people":"ppl",
+    "sorry":"sowwy",
+    "like":"liek"
+    
+}
+subs = {
+    "ie":"ei",
+    "le":"el",
+    "in":"ni",
+    "o":"0",
+    "t":"7",
+    "e":"3",
+    "l":"|"
+}
+
+addons = "lol idk wut rofl ikr".split()
